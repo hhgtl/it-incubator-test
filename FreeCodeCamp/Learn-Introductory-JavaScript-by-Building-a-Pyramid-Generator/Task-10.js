233a5bcf9d8fb4ccc19a1cae481615331699b463
Petro <petrosahal66@gmail.com>
2024-10-12 00:02:07 +0300

    new task-10 in freecodecamp folder

---
+++ FreeCodeCamp/Learn-Introductory-JavaScript-by-Building-a-Pyramid-Generator/Task-10.js
@@ -0,0 +1,19 @@
+// DESCRIPTION
+
+// The default value of an uninitialized variable is undefined. This is a special data type that represents a value that does not have a definition yet.
+
+// You can still assign a value to an uninitialized variable. Here is an example:
+
+// Example Code
+// let uninitialized;
+// uninitialized = "assigned";
+// Assign the string "Test" to your secondCharacter variable below your declaration. Open the console to see how your log has changed.
+
+// SOLUTION
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+let secondCharacter;
+secondCharacter = 'Test';
+console.log(secondCharacter);
