078056e9d26334a79e17eb2cffc88279abcd352b
Petro <petrosahal66@gmail.com>
2024-10-11 23:47:43 +0300

    new task-6 in freecodecamp folder

---
+++ FreeCodeCamp/Learn-Introductory-JavaScript-by-Building-a-Pyramid-Generator/Task-6.js
@@ -0,0 +1,15 @@
+// DESCRIPTION
+// When a variable is declared with the let keyword, you can reassign (or change the value of) that variable later on. In this example, the value of programmer is changed from "Naomi" to "CamperChan".
+
+// Example Code
+// let programmer = "Naomi";
+// programmer = "CamperChan";
+// Note that when reassigning a variable, you do not use the let keyword again.
+
+// After your console.log, assign the value "World" to your character variable.
+
+// SOLUTION
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
