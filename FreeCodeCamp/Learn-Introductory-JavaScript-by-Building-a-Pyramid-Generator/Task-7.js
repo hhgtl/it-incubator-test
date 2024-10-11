71404da3e089dbdf8c67934ad332276fa71d159f
Petro <petrosahal66@gmail.com>
2024-10-11 23:49:59 +0300

    new task-7 in freecodecamp folder

---
+++ FreeCodeCamp/Learn-Introductory-JavaScript-by-Building-a-Pyramid-Generator/Task-7.js
@@ -0,0 +1,10 @@
+// DESCRIPTION
+
+// Now log your character variable to the console again. You should see the string "Hello", then the string "World", in the console.
+
+// SOLUTION
+
+let character = 'Hello';
+console.log(character);
+character = 'World';
+console.log(character);
