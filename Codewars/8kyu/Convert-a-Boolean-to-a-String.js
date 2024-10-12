8a16980203299e763ff1c571079456a5943acc2f
Petro <petrosahal66@gmail.com>
2024-10-12 10:54:30 +0300

    new file 'Convert-a-Boolean-to-a-String' in codewars/8kyu folder

---
+++ Codewars/8kyu/Convert-a-Boolean-to-a-String.js
@@ -0,0 +1,13 @@
+// DESCRIPTION
+
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+
+// SOLUTION
+
+function booleanToString(b) {
+  return String(b);
+}
+
+console.log(booleanToString(true));
+console.log(typeof booleanToString(true));
