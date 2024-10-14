5be1953567ec8116971bd0313f2aa3109acbd663
Petro <petrosahal66@gmail.com>
2024-10-14 23:25:55 +0300

    new file 'MakeUpperCase' in codewars/8kyu folder

---
+++ Codewars/8kyu/MakeUpperCase.js
@@ -0,0 +1,8 @@
+// DESCRIPTION
+// Write a function which converts the input string to uppercase.
+// SOLUTION
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+console.log(makeUpperCase('hello'));
